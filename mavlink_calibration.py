2ca66d460a22fdd0e24902f852c02808b99f7a19
Arthur Golubtsov <goldartt@gmail.com>
2019-08-30 21:21:21 +0300

    Change description

--- mavlink_calibration.py
+++ mavlink_calibration.py
@@ -85,7 +85,7 @@ def wait_heartbeat(connection, timeout_to_reconnect=2.):
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Send calibration command to mavlink device. Options are: gyro or level.')
+    parser = argparse.ArgumentParser(description='Send calibration command to px4 device. Options are: gyro or level.')
     parser.add_argument('hostname', nargs='?', default='192.168.11.1',
                         help="Address for connection")
     parser.add_argument('--port', default=5760, type=int,
