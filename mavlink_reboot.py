bb51bf33d668f0c9c2f70aa922851fd770e5d630
Arthur Golubtsov <goldartt@gmail.com>
2019-09-02 16:04:30 +0300

    Update mavlink_reboot to wait ack message

--- mavlink_reboot.py
+++ mavlink_reboot.py
@@ -1,24 +1,42 @@
 from pymavlink import mavutil
 import argparse
+import time
+from mavlink_calibration import set_connection, wait_heartbeat, check_ack
 
+def reboot(connection, delay=0.01, wait_ack=True, timeout=2.):
+    print('Send reboot message')
+    ack = False
+    start_time = time.time()
+    confirmation = 0
+    while not ack:
+        connection.reboot_autopilot()
+        if wait_ack:
+            print('Send MAV_CMD_PREFLIGHT_REBOOT_SHUTDOWN, attempt {}'.format(confirmation))
+            ack = check_ack(mavutil.mavlink.MAV_CMD_PREFLIGHT_REBOOT_SHUTDOWN, connection) or not wait_ack
+            confirmation = (confirmation + 1) % 255
+            if time.time() - start_time > timeout:
+                print('Send calibration start is timed out, try to reconnect')
+                connection.reconnect()
+                start_time = time.time()
+        else: 
+            ack = True
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Send calibration command to px4 device. Options are: gyro or level.')
     parser.add_argument('hostname', nargs='?', default='192.168.11.1',
-                        help="Filename of route csv table")
-    parser.add_argument('--tries', default=3, type=int,
-                        help="Number of tries, default is 3")
+                        help="Address for connection")
+    parser.add_argument('--port', default=5760, type=int,
+                        help="Port, default is 5760")
+    parser.add_argument('--type', default='tcp', type=str,
+                        help="Type of connection, default is tcp")
 
+    # Assemble connection string
     args = parser.parse_args()
+    connection_string = "{}:{}:{}".format(args.type,args.hostname,args.port)
 
-    # Start a connection listening to a UDP port
-    the_connection = mavutil.mavlink_connection('tcp:{}:5760'.format(args.hostname), input=False)
+    # Start a connection and wait first heartbeat
+    connection, heartbeat = set_connection(connection_string)
+    print("Got heartbeat from {}: system {} component {}".format(args.hostname, connection.target_system, connection.target_system))
 
-    # Wait for the first heartbeat 
-    #   This sets the system and component ID of remote system for the link
-    the_connection.wait_heartbeat()
-    print("Heartbeat from {}: system {} component {}".format(args.hostname, the_connection.target_system, the_connection.target_system))
-
-    for i in range(args.tries):
-        the_connection.reboot_autopilot()
+    reboot(connection)
